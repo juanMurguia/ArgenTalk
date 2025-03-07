8f0ec0ba07833ae02f99e5f101915c3a65b77bc5
Juan <juancruzmur@gmail.com>
2025-03-07 16:13:25 -0300

    actualizo packages

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import './App.css';
 import Container from './components/Container';
 import ContainerTranslate from './components/ContainerTranslate';
 import Header from './components/Header';
-
-
+import React from 'react';
 
 function App() {
   return (
